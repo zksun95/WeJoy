1c81f9ec824f4d1f622b92a00042e24aba516957
zksun95 <zksun95@gmail.com>
2018-07-01 18:31:12 -0700

    login and signup error

--- wejoy/client/src/app/components/sign-in/sign-in.component.ts
+++ wejoy/client/src/app/components/sign-in/sign-in.component.ts
@@ -14,6 +14,7 @@ export class SignInComponent implements OnInit {
 
   email: string;
   password: string;
+  error: string = "";
 
   ngOnInit() {
     if(this.auth.isAuthenticated()){
@@ -22,17 +23,38 @@ export class SignInComponent implements OnInit {
   }
 
   login(){
-    let data = JSON.stringify({
-      "email": this.email,
-      "password": this.password
-    });
-    this.auth.login(data).subscribe((response)=>{
-      //console.log(response);
-      this.auth.setUser(response['token'], JSON.stringify(response['user']));
-      this.router.navigate(['/home']);
-    });
-    console.log(this.email);
-    console.log(this.password);
+    this.error = "";
+    if(!this.password){
+      this.error = "Please enter your password."
+    }
+    if(!this.email){
+      this.error = "Please enter your username."
+    }
+    if(!this.error){
+      let data = JSON.stringify({
+        "email": this.email,
+        "password": this.password
+      });
+      this.auth.login(data).subscribe(
+        (response)=>{
+          //console.log(response);
+          this.auth.setUser(response['token'], JSON.stringify(response['user']));
+          this.router.navigate(['/home']);
+        },
+        (error)=>{
+          console.log(error);
+          if(error['error']['email']){
+            this.error = error['error']['email'];
+          }
+          if(error['error']['message']){
+            this.error = error['error']['message'];
+          }
+        }
+    );
+      //console.log(this.email);
+      //console.log(this.password);
+    }
+    
   }
 
 }
--- wejoy/client/src/app/components/sign-up/sign-up.component.ts
+++ wejoy/client/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, DoCheck, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,6 +16,7 @@ export class SignUpComponent implements OnInit {
   password: string = "";
   password_: string = "";
   agree: boolean = false;
+  error: string = "";
 
   ngOnInit() {
     if(this.auth.isAuthenticated()){
@@ -23,18 +24,52 @@ export class SignUpComponent implements OnInit {
     }
   }
 
+  ngDoCheck(){
+    if (this.agree && this.error=="Please agree ..."){
+      this.error = "";
+    }
+    if (this.username && this.error == "Enter a username."){
+      this.error = "";
+    }
+  }
+
   signup(){
-    let data = JSON.stringify({
-      "email": this.email,
-      "username": this.username,
-      "password": this.password
-    });
-    //console.log(data);
-    this.auth.signup(data).subscribe((response)=>{
-      //console.log(response['user']);
-      this.auth.setUser(response['token'], JSON.stringify(response['user']));
-      this.router.navigate(['/home']);
-    });
+    this.error = "";
+    if (!this.agree){
+      this.error = "Please agree ...";
+    }
+    if (this.password!=this.password_ && this.password!=""){
+      this.error = "Passwords are different.";
+    }
+    if (!this.password){
+      this.error = "Enter password."
+    }
+    if (!this.username){
+      this.error = "Enter a username.";
+    }
+    if (!this.email){
+      this.error = "Enter an eamil.";
+    }
+    if (!this.error){
+      let data = JSON.stringify({
+        "email": this.email,
+        "username": this.username,
+        "password": this.password
+      });
+      //console.log(data);
+      this.auth.signup(data).subscribe(
+        (response)=>{
+        //console.log(response['user']);
+        this.auth.setUser(response['token'], JSON.stringify(response['user']));
+        this.router.navigate(['/home']);
+        },
+        (error)=>{
+          console.log(error['error']['errors']);
+          this.error = error['error']['errors']['email'];
+        }
+      );
+    }
+    
   }
 
 }
